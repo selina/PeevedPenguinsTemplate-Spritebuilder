48ddfe647cf34c345e16603cd6435a1f50f37572
Selina <enigmacat@users.noreply.github.com>
2014-06-27 15:38:20 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -28,6 +28,7 @@ CCPhysicsJoint *_penguinCatapultJoint;
     _physicsNode.debugDraw = true;
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
+    _physicsNode.collisionDelegate = self;
 }
 
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
@@ -94,5 +95,10 @@ CCPhysicsJoint *_penguinCatapultJoint;
 
 -(void)retry {
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
 }
 @end
